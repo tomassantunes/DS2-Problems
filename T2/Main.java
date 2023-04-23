9c23b154a395d130b4b8558c8caa1132471c2b1f
Tomás Antunes <73246184+tomassantunes@users.noreply.github.com>
2023-04-23 18:12:28 +0100

    resolvido? Mooshak -^> Time limit Exceeded!!!

--- T2/Main.java
+++ T2/Main.java
@@ -15,23 +15,23 @@ class Main {
         int C = Integer.parseInt(tmp[1]); // columns
         int T = Integer.parseInt(tmp[2]); // test cases
 
-        String[][] map = new String[R][C];
+        String[][] map = new String[R+2][C+2];
        
         int nodes = 0;
         for(int i = 0; i < R; i++) {
             tmp = input.readLine().split("");
             for(int j = 0; j < C; j++) {
-                map[i][j] = tmp[j];
-                if(map[i][j].compareTo(".") == 0 || map[i][j].compareTo("H") == 0) nodes++;
+                map[i+1][j+1] = tmp[j];
+                if(map[i+1][j+1].equals(".") || map[i+1][j+1].equals("H")) nodes++;
             }
         }
 
-        Dream dream = new Dream(R, C, nodes, map);
+        Dream dream = new Dream(R+2, C+2, nodes, map);
 
         for(; T > 0; T--) {
             tmp = input.readLine().split(" ");
 
-            int steps = dream.escape(dream.getPoint(new Point(Integer.parseInt(tmp[0]) - 1, Integer.parseInt(tmp[1]) - 1)));
+            int steps = dream.escape(dream.getPoint(new Point(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]))));
 
             if(steps == -1)
                 System.out.println("Stuck.");
@@ -47,11 +47,12 @@ class Dream {
     int nodes;
     String[][] map;
     List<Point>[] adj;
-
-    Point hole;
+    Point[] pontos;
 
     int[][] pointMap;
     int c = 1;
+
+    Point hole;
 
     @SuppressWarnings("unchecked")
     public Dream(int rows, int columns, int nodes, String[][] map) {
@@ -61,6 +62,7 @@ class Dream {
         this.map = map;
 
         pointMap = new int[rows][columns];
+        pontos = new Point[nodes];
 
         adj = new List[nodes];
         for(int i = 0; i < nodes; i++) {
@@ -68,6 +70,7 @@ class Dream {
         }
 
         buildGraph();
+        // printGraph();
     }
 
     public int getPoint(Point u) {
@@ -76,65 +79,86 @@ class Dream {
             c++;
         }
 
+        pontos[pointMap[u.x][u.y] - 1] = u;
+
         return pointMap[u.x][u.y] - 1;
     }
 
     public void addEdge(Point u, Point v) {
-        if(map[v.x][v.y].compareTo("H") == 0) {
-            v.isHole = 1;
+        if(v.x == -1 && v.y == -1) return;
+
+        if(map[v.x][v.y].equals("H")) {
             hole = v;
         }
 
         adj[getPoint(u)].add(v);
     }
 
-    /* TODO - FIX
-     * Deve construir um grafo, que vai representar todos
-     * os caminhos possíveis de acordo com os bloqueios e limites do mapa
-    */
+    public Point buildArch(int x, int y, int v1, int v2) {
+        int xi = x;
+        int yi = y;
+
+        while(x < rows && x > 0 && y < columns && y > 0) {
+            String tmp = map[x+v1][y+v2];
+            if(tmp == null) return new Point(NONE, NONE);
+
+            if(tmp.equals(("H"))) {
+                return new Point(x+v1, y+v2);
+            }
+
+            if(tmp.equals("O")) {
+                break;
+            }
+
+            if(tmp.equals(".")) {
+                x += v1;
+                y += v2;
+            }
+        }
+
+        if(x == xi && y == yi) return new Point(NONE, NONE);
+        return new Point(x, y);
+    }
 
     public void buildGraph() {
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < columns; j++) {
-                Point u = new Point(i, j);
-                if(map[i][j].compareTo(".") == 0) {
-                    if(i > 0 && (map[i-1][j].compareTo(".") == 0 || map[i-1][j].compareTo("H") == 0)) {
-                        addEdge(u, new Point(i-1, j));
-                    }
-
-                    if(i < rows - 1 && (map[i+1][j].compareTo(".") == 0 || map[i+1][j].compareTo("H") == 0)) {
-                        addEdge(u, new Point(i+1, j));
-                    }
-
-                    if(j > 0 && (map[i][j-1].compareTo(".") == 0 || map[i][j-1].compareTo("H") == 0)) {
-                        addEdge(u, new Point(i, j-1));
-                    }
-
-                    if(j < columns - 1 && (map[i][j+1].compareTo(".") == 0 || map[i][j+1].compareTo("H") == 0)) {
-                        addEdge(u, new Point(i, j+1));
-                    }
+        for(int i = 1; i < rows - 1; i++) {
+            for(int j = 1; j < columns - 1; j++) {
+                if(map[i][j].equals(".")) {
+                    addEdge(new Point(i, j), buildArch(i, j, 1, 0));
+                    addEdge(new Point(i, j), buildArch(i, j, -1, 0));
+                    addEdge(new Point(i, j), buildArch(i, j, 0, 1));
+                    addEdge(new Point(i, j), buildArch(i, j, 0, -1));
                 }
             }
         }
     }
 
+    void printGraph() {
+        for(int i = 0; i < nodes; i++) {
+            if(pontos[i] == null) break;
+            System.out.print(pontos[i].x + " " + pontos[i].y + " -> ");
+            for(var y : adj[i]) {
+                System.out.print(y.x + " " + y.y + "   ");
+            }
+            System.out.println();
+
+        }
+    }
+
     public static final int INFINITY = Integer.MAX_VALUE;
     public static final int NONE = -1;
-    private static enum Colour { WHITE, GREY, BLACK };
 
     public int escape(int s) {
-        Colour[] colour = new Colour[nodes];
+        int[] done = new int[nodes];
         int[] d = new int[nodes];
-        int[] p = new int[nodes];
 
         for(int u = 0; u < nodes; u++) {
-            colour[u] = Colour.WHITE;
+            done[u] = 0;
             d[u] = INFINITY;
-            p[u] = NONE;
         }
 
-        colour[s] = Colour.GREY;
-        d[s] = NONE;
+        done[s] = 1;
+        d[s] = 0;
 
         Queue<Integer> Q = new LinkedList<>();
         Q.add(s);
@@ -142,41 +166,31 @@ class Dream {
         while(!Q.isEmpty()) {
             int u = Q.remove();
 
-            /* TODO
-             * de momento aumentei o número de nodes para o número
-             * de casas vazias mais o número de buracos
-            */
-
             for(Point v : adj[u]) {
                 int vi = getPoint(v);
-                if(colour[vi] == Colour.WHITE) {
-                    colour[vi] = Colour.GREY;
+                if(done[vi] == 0) {
+                    done[vi] = 1;
                     d[vi] = d[u] + 1;
-                    Q.add(vi);
+
+                    if(!v.equals(hole))
+                        Q.add(vi);
                 }
             }
-
-            colour[u] = Colour.BLACK;
         }
 
-        if(d[getPoint(hole)] == 0)
+        int dHole = d[getPoint(hole)];
+        if(dHole == Integer.MAX_VALUE)
             return -1;
-        return d[getPoint(hole)];
+        return dHole;
     }
 }
 
 class Point {
     public int x;
     public int y;
-    public int isHole;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
-        this.isHole = 0;
-    }
-
-    public Point sum(Point other) {
-        return new Point(this.x + other.x, this.y + other.y);
     }
 }
