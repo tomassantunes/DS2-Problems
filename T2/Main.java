8df93266cdda054427688282c9cd4eeacb47a584
Tomás Antunes <73246184+tomassantunes@users.noreply.github.com>
2023-04-24 13:14:34 +0100

    strings poh cuts

--- T2/Main.java
+++ T2/Main.java
@@ -15,23 +15,18 @@ class Main {
         int C = Integer.parseInt(tmp[1]); // columns
         int T = Integer.parseInt(tmp[2]); // test cases
 
-        String[][] map = new String[R+2][C+2];
+        char[][] map = new char[R][C];
        
-        int nodes = 0;
         for(int i = 0; i < R; i++) {
-            tmp = input.readLine().split("");
-            for(int j = 0; j < C; j++) {
-                map[i+1][j+1] = tmp[j];
-                if(map[i+1][j+1].equals(".") || map[i+1][j+1].equals("H")) nodes++;
-            }
+            map[i] = input.readLine().toCharArray();
         }
 
-        Dream dream = new Dream(R+2, C+2, nodes, map);
+        Dream dream = new Dream(R, C, map);
 
         for(; T > 0; T--) {
             tmp = input.readLine().split(" ");
 
-            int steps = dream.escape(dream.getPoint(new Point(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]))));
+            int steps = dream.escape(Integer.parseInt(tmp[0]) - 1, Integer.parseInt(tmp[1]) - 1);
 
             if(steps == -1)
                 System.out.println("Stuck");
@@ -45,7 +40,7 @@ class Dream {
     int rows;
     int columns;
     int nodes;
-    String[][] map;
+    char[][] map;
     List<Point>[] adj;
 
     int[][] pointMap;
@@ -54,10 +49,10 @@ class Dream {
     Point hole;
 
     @SuppressWarnings("unchecked")
-    public Dream(int rows, int columns, int nodes, String[][] map) {
+    public Dream(int rows, int columns, char[][] map) {
         this.rows = rows;
         this.columns = columns;
-        this.nodes = nodes;
+        this.nodes = rows * columns;
         this.map = map;
 
         pointMap = new int[rows][columns];
@@ -82,7 +77,7 @@ class Dream {
     public void addEdge(Point u, Point v) {
         if(v.x == -1 && v.y == -1) return;
 
-        if(map[v.x][v.y].equals("H")) {
+        if(map[v.x][v.y] == 'H') {
             hole = v;
         }
 
@@ -93,28 +88,27 @@ class Dream {
         int xi = x;
         int yi = y;
 
-        while(x < rows && x > 0 && y < columns && y > 0) {
-            String tmp = map[x+v1][y+v2];
-            if(tmp == null) return new Point(NONE, NONE);
+        while(x+v1 >= 0 && x+v1 < rows && y+v2 >= 0 && y+v2 < columns) {
+            char tmp = map[x+v1][y+v2];
 
-            if(tmp.equals(".")) {
+            if(tmp == '.') {
                 x += v1;
                 y += v2;
-            } else if(tmp.equals("O")) {
-                break;
-            } else if(tmp.equals(("H"))) {
+            } else if(tmp == 'O') {
+                if(x == xi && y == yi) break;
+                return new Point(x, y);
+            } else if(tmp == 'H') {
                 return new Point(x+v1, y+v2);
             }
        }
 
-        if(x == xi && y == yi) return new Point(NONE, NONE);
-        return new Point(x, y);
+        return new Point(NONE, NONE);        
     }
 
     public void buildGraph() {
-        for(int i = 1; i < rows - 1; i++) {
-            for(int j = 1; j < columns - 1; j++) {
-                if(map[i][j].equals(".")) {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                if(map[i][j] == '.') {
                     addEdge(new Point(i, j), buildArch(i, j, 1, 0));
                     addEdge(new Point(i, j), buildArch(i, j, -1, 0));
                     addEdge(new Point(i, j), buildArch(i, j, 0, 1));
@@ -127,7 +121,8 @@ class Dream {
     public static final int INFINITY = Integer.MAX_VALUE;
     public static final int NONE = -1;
 
-    public int escape(int s) {
+    public int escape(int sx, int sy) {
+        int s = getPoint(new Point(sx, sy));
         int[] color  = new int[nodes];
         int[] d = new int[nodes];
 
